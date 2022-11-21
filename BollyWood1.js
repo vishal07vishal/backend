0249441b793f29a02f033508f218cee8a3df24b9
vishal <104211252+vishal07vishal@users.noreply.github.com>
2022-11-21 12:52:21 +0530

    Add files via upload

---
+++ BollyWood1.js
@@ -0,0 +1,31 @@
+const BollyWood1=[
+    {
+        "id": 1,
+        "title": "Ranveer Singh",
+        "Image": "https://stat1.bollywoodhungama.in/wp-content/uploads/2022/08/ranveer-620.jpg",
+        "category": "Bollywood",
+        "description": "The recently held 67th Filmfare Awards was truly a festival of Indian Cinema with many films bagging some prestigious awards. From Shershaah to Sardar Udham, some of these films won big, bagging most of the technical awards. And the Best Actor award was given to Ranveer Singh for his portrayal of Kapil Dev in the sports-based biopic ’83 which also featured Deepika Padukone as his reel-life wife Romi Dev."
+      },
+      {
+        "id": 51,
+        "title": "Katrina Kaif reveals reason behind secret wedding with Vicky Kaushal",
+        "Image": "https://images.hindustantimes.com/img/2022/08/31/550x309/vicky_katrina_1661918979656_1661918992710_1661918992710.jpg",
+        "category": "Bollywood",
+        "description": "Katrina Kaif and Vicky Kaushal tied the knot at a resort in Sawai Madhopur, Rajasthan on December 9 last year. The wedding was a highly guarded and private affair and wasn't confirmed by the couple until they shared their first pictures as a bride and groom. Katrina has now shared the reason why they chose to remain quiet about their wedding.Vicky Kaushal sings Katrina Kaif's Kala Chashma as they walk their first red carpet together"
+      },
+      {
+        "id": 2,
+        "title": "Kajol wears red saree for Ganesh Chaturthi; Kareena, Alia wish fans. See pics",
+        "Image": "https://images.hindustantimes.com/img/2022/08/31/550x309/Untitled_design_(11)_1661931982870_1661931989441_1661931989441.png",
+        "category": "Bollywood",
+        "description": "Bollywood celebrities celebrated Ganesh Chaturthi by sharing photos and messages with their fans on social media. On Wednesday, while many wished their fans on the occasion, some dropped photos from their celebration. Kajol shared a photo of herself on Ganesh Chaturthi, while Kareena Kapoor and Alia Bhatt sent love to their fans. Divya Dutta on Ganesh Chaturthi 2022: I get too attached every time Bappa comes home."
+      },
+      {
+        "id": 61,
+        "title": "When Shah Rukh Khan's duplicate shot instead of him for Pardes' Yeh Dil Deewana",
+        "Image": "https://images.hindustantimes.com/img/2022/08/31/550x309/f5d3700a-71f8-46f0-a8e8-2b3902fedcf9_1661930840404_1661930853885_1661930853885.jpg",
+        "category": "footer1",
+        "description": " Shah Rukh Khan's Pardes was released in 1997. The film was directed by Subhash Ghai, who revealed in an old interview that a part of film's hit song Yeh Dil Deewana was shot by SRK's duplicate. Subhash said that at that time, Shah Rukh's wife Gauri Khan was pregnant and hence Shah Rukh had to leave midway. Subhash Ghai says Pardes was first offered to Madhuri Dixit, reveals why Mahima Choudhry got the role."
+      }
+]
+module.exports = BollyWood1;
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const BollyWood1=require('./BollyWood1')
+
+app.get('/Bollywood',(req,res)=>{
+    res.json(BollyWood1).status(200)
+    res.end();
+
+})
+
+app.listen(3000,()=>{
+    console.log('connected to 3000')
+})
