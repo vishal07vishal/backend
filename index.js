fae67ec0dd571922d0ecb7380921472f115093d5
vishal <104211252+vishal07vishal@users.noreply.github.com>
2022-11-24 12:25:12 +0530

    Back-end

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bollyRouter = require('./route/data')
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+app.use("/api",bollyRouter)
+
+
+app.listen(process.env.PORT||8000,()=>{
+    console.log("app is running");
+})
